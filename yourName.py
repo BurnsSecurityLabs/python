28bb67e241aa04019c5795028aea35a9986a63a9
woimoli1979 <redburns28@gmail.com>
2026-01-20 10:19:56 -0500

    pushing break block

--- yourName.py
+++ yourName.py
@@ -1,1 +1,9 @@
+#testing break
+yourName = 'Melissa'
+while True:
+    print('Please type your name:')
+    name = input()
+    if name == yourName:
+        print('Thank you, ' + name)
+        break
 
