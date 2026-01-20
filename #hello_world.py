8fdac7936ff3d9c422f587f202e5b37667c826ec
woimoli1979 <redburns28@gmail.com>
2026-01-20 09:49:23 -0500

    Update hello_word

--- #hello_world.py
+++ #hello_world.py
@@ -1,4 +1,11 @@
 #hello_world
 
-print("Hello, Melissa! You have connected Visual Studio Pro to Github. Congratulations!!")
+print("Hello! You have connected Visual Studio Pro to Github. Congratulations!!")
 
+print('What is your name?')
+fName = input()
+print ("hello " + str(fName))
+print("The length of your name is " + str(len(fName)))
+print("What is your age?")
+age = input()
+print("You will be " + str(int(age) + 1) + ' in one year.')
